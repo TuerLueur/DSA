6aa41870206a43a2cb161b6878943399a5bf58bf
TuerLueur <1477374603@qq.com>
2020-03-23 19:51:23 +0800

    'binSearch'

---
+++ binSearch.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int binSearch(int* S, int e, int lo, int hi){
+    while (lo < hi){    // 切记while终止条件
+        int mi = (lo + hi) >> 1;
+        (e < S[mi]) ? hi = mi : lo = mi + 1;
+    }
+    return --lo;
+}
+
+int main()
+{
+    int S[] = {1, 2, 3, 4, 5, 6, 9};
+    printf("The pos of 7 is: %d \n", binSearch(S, 7, 0, 7));
+    printf("The pos of 0 is: %d \n", binSearch(S, 0, 0, 7));
+    printf("The pos of 6 is: %d \n", binSearch(S, 6, 0, 7));
+    return 0;
+}
